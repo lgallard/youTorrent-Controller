ef281472c88c1552b59382d18012abbc490348ec
Luis Gallardo <luis.gallardo@smartmatic.com>
2016-04-07 12:51:16 -0430

    *  Fixed the unsafe X509TrustManager implementation

--- app/src/main/java/com/lgallardo/youtorrentcontroller/MySSLSocketFactory.java
+++ app/src/main/java/com/lgallardo/youtorrentcontroller/MySSLSocketFactory.java
@@ -32,20 +32,12 @@ public class MySSLSocketFactory extends SSLSocketFactory {
     public MySSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
         super(truststore);
 
-        TrustManager tm = new X509TrustManager() {
-            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-            }
 
-            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-            }
-
-            public X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-        };
+        TrustManager tm = new MyTrustManager(truststore);
 
         sslContext.init(null, new TrustManager[] { tm }, null);
     }
+
 
     @Override
     public Socket createSocket(Socket socket, String host, int port, boolean autoClose) throws IOException, UnknownHostException {
